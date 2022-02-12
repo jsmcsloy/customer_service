840f632f5fc2716b9f0ed7b16070a8479c69c8d4
Jsm <jmcsloy@gmail.com>
2022-02-12 16:13:05 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from datetime import datetime
+import datetime as dt
+
+#set date & day format
+today = dt.datetime.today()
+days = ["Monday", "Tuesday", "Wednesday",
+        "Thursday", "Friday", "Saturday", "Sunday"]
+now = dt.datetime.now()
+format = "%d/%m/%Y"
+date1 = now.strftime(format)
+todays_date = days[today.weekday()],date1
+
+
+
+#set up title and basic skeleton
+st.title("Customer Service Tracker") 
+
+try:
+    file = st.file_uploader("Upload and visualise the data ... .xlxs/csv")
+
+
+    df = pd.read_excel(file)
+    #mask_matched = df.isna(df["Date on database"])
+    df = df[df["Date on database"].isnull()]
+
+    matched = df
+    #late matches
+    late = matched[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]]
+
+
+    late['Our Due Date'] = late['Our Due Date'].dt.strftime('%d-%m-%Y')
+    late = late[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]].dropna()
+    late = late.sort_values(by='Our Due Date')
+    mask = (df["Our Due Date"] >= now)
+    filtered = df=df.loc[mask]
+
+    #Due matches
+    df['Our Due Date'] = df['Our Due Date'].dt.strftime('%d-%m-%Y')
+    df[["Brand Reference","Product Line"]] = df[["Brand Reference", "Product Line"]].astype(str)
+    due = df[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]].dropna()
+    due = due.sort_values(by='Our Due Date')
+
+    st.header("Matches due  " + todays_date[0] +" " + todays_date[1])
+    st.write(due)
+
+    st.header("Overdue matches  " )
+    st.write(late)
+
+except: 
+    pass
+
+
+# try:
+# 	df = pd.read_excel(file)
+# except:
+# 	df = pd.read_csv(file)
+
+# finally:
+# 	st.write("error")
+
+
