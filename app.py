baaec36b0892400dafcd03a15ad5f632f27bfa7f
Jsm <jmcsloy@gmail.com>
2022-02-12 17:04:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import datetime as dt
+import openpyxl
+import pandas as pd
+from pandas import DataFrame, read_csv, read_excel
+import base64
+import os
+
+
+
 
 #set date & day format
 today = dt.datetime.today()
@@ -23,39 +31,47 @@ except:
     pass
 
 
-
+try:
    
-df = pd.read_excel(file)
-#mask_matched = df.isna(df["Date on database"])
-df = df[df["Date on database"].isnull()]
+    df = pd.read_excel(file)
+    #mask_matched = df.isna(df["Date on database"])
+    df = df[df["Date on database"].isnull()]
+    
+    st.write("pass1")
 
-st.write("pass1")
-
-matched = df
-#late matches
-late = matched[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]]
-
-
-late['Our Due Date'] = late['Our Due Date'].dt.strftime('%d-%m-%Y')
-late = late[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]].dropna()
-late = late.sort_values(by='Our Due Date')
-mask = (df["Our Due Date"] >= now)
-filtered = df=df.loc[mask]
-
-#Due matches
-df['Our Due Date'] = df['Our Due Date'].dt.strftime('%d-%m-%Y')
-df[["Brand Reference","Product Line"]] = df[["Brand Reference", "Product Line"]].astype(str)
-due = df[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]].dropna()
-due = due.sort_values(by='Our Due Date')
-
-st.header("Matches due  " + todays_date[0] +" " + todays_date[1])
-st.write(due)
-
-st.header("Overdue matches  " )
-st.write(late)
+    matched = df
+    #late matches
+    late = matched[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]]
 
 
+    late['Our Due Date'] = late['Our Due Date'].dt.strftime('%d-%m-%Y')
+    late = late[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]].dropna()
+    late = late.sort_values(by='Our Due Date')
+    mask = (df["Our Due Date"] >= now)
+    filtered = df=df.loc[mask]
+
+    #Due matches
+    df['Our Due Date'] = df['Our Due Date'].dt.strftime('%d-%m-%Y')
+    df[["Brand Reference","Product Line"]] = df[["Brand Reference", "Product Line"]].astype(str)
+    due = df[["Our Due Date","Service","Product Line","Brand Reference", "Colourist"]].dropna()
+    due = due.sort_values(by='Our Due Date')
+
+    st.header("Matches due  " + todays_date[0] +" " + todays_date[1])
+    st.write(due)
+
+    st.header("Overdue matches  " )
+    st.write(late)
+
+except: 
+    st.write("Error processing file, try again with a differnt file ")
 
 
+# try:
+# 	df = pd.read_excel(file)
+# except:
+# 	df = pd.read_csv(file)
 
- 
+# finally:
+# 	st.write("error")
+
+
